93306153767874062fd8bc7bb72fbbfdb017220b
nkturtas@gmx.com <nkturtas@gmx.com>
2024-08-17 09:29:59 +0300

    Add project files.

---
+++ RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RockPaperScissors
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(842, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rock Paper Scissors";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(139, 93);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(275, 266);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(136, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 52);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Computer";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(429, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 52);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Player";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(284, 478);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 56);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(429, 93);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(275, 266);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(888, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "form1";
+            this.Text = "Rock Paper Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    
+    public partial class form1 : Form
+    {
+        Random random = new Random();
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int computerTurn = random.Next(1, 4);
+            int playerTurn = random.Next(1, 4);
+
+            switch (computerTurn)
+            {
+                case 1:
+                    pictureBox2.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Rock.png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+                    pictureBox2.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Paper.png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 3:
+                    pictureBox2.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Scissors.png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                default:
+                    pictureBox2.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Rock.png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+            }
+
+            switch (playerTurn)
+            {
+                case 1:
+                    pictureBox3.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Rock.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+                    pictureBox3.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Paper.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 3:
+                    pictureBox3.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Scissors.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                default:
+                    pictureBox3.ImageLocation = @"D:\C# Projects\Windows Forms Projects\10 Rock Paper Scissors\Images\Rock.png";
+                    pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+            }
+
+            determineWinner(computerTurn, playerTurn);
+        }
+        public void determineWinner(int computerScore, int playerScore)
+        {
+            if (computerScore == 1 && playerScore == 2)
+            {
+                label1.Text = "You win, congrats!";
+            }
+            else if (computerScore == 1 && playerScore == 3)
+            {
+                label1.Text = "You lose, the computer wins!";
+            }
+            else if (computerScore == 2 && playerScore == 1)
+            {
+                label1.Text = "You lose, the computer wins!";
+            }
+            else if (computerScore == 2 && playerScore == 3)
+            {
+                label1.Text = "You win, congrats!";
+            }
+            else if (computerScore == 3 && playerScore == 1)
+            {
+                label1.Text = "You win, congrats!";
+            }
+            else if (computerScore == 3 && playerScore == 2)
+            {
+                label1.Text = "You lose, the computer wins!";
+            }
+            else
+            {
+                label1.Text = "There is a tie!";
+            }
+
+        }
+    }
+}
